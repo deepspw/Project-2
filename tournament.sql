cba3b0182030ddb7fb348c2f98b5c859a8cd1314
ttye <tomball.me@icloud.com>
2015-07-23 23:32:30 +0100

    added sql views

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,19 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Raw played 
+create view rawplayed as select p1, count(*) 
+	from (select p1 from matches union all select p2 from matches) as raw 
+	group by p1;
+
+-- Played games view
+create view played as select players.id, rawplayed.p1, rawplayed.count 
+	from players 
+	left outer join rawplayed on rawplayed.p1 = players.id;
+
+-- Wincount view
+create view wincount as select players.id, matches.winner, count(*) as wins 
+	from players 
+	left outer join matches on matches.winner=players.id 
+	group by players.id, matches.winner;
 
